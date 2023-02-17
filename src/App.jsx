e847ab3ddae298200b0919eb62e34de60abbd029
ThiefGang <104874053+ThiefGang@users.noreply.github.com>
2023-02-17 10:59:22 +0200

    connect front end with backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from "react"
+
+import { Routes, Route, useNavigate } from "react-router-dom"
+import * as puppyService from "./services/puppyService"
 
 // Components
-import Nav from './components/Nav'
-import Landing from './pages/Landing/Landing'
+import Nav from "./components/Nav"
+import Landing from "./pages/Landing/Landing"
 
 const App = () => {
+  const [puppies, setPuppies] = useState([])
+
+  useEffect(() => {
+    const fetchPuppies = async () => {
+      const data = await puppyService.index()
+      setPuppies(data)
+    }
+    fetchPuppies()
+  }, [])
+
+  //useEffect for console logging
+  useEffect(()=>{
+    console.log(puppies);
+  },[puppies])
+
   return (
     <>
       <Nav />
       <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/puppies' element={
-          <h1>PUPPY LIST</h1>
-        } />
+        <Route path="/" element={<Landing />} />
+        <Route path="/puppies" element={<h1>PUPPY LIST</h1>} />
       </Routes>
     </>
   )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,4 +12,4 @@ const Nav =  () => {
   )
 }
 
-export default Nav
+export default Nav 
